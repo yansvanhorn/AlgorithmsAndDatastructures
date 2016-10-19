9aae108f9eaeb8b9d8bd1c6a31e7aaf727a86d8d
yansvanhorn <marcin.bazarnik@gmail.com>
2016-10-19 15:27:30 +0200

    Closest (final)

--- week4/src/main/java/Closest.java
+++ week4/src/main/java/Closest.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.util.*;
 
 import static java.lang.Math.*;
+import static java.util.Comparator.comparingDouble;
+import static java.util.Comparator.comparingInt;
 
 class Point implements Comparable<Point> {
     int x, y;
@@ -42,6 +44,27 @@ class Point implements Comparable<Point> {
     }
 }
 
+class Result {
+    double distance;
+    Point[] leftByY;
+    Point[] rightByY;
+
+    public Result(double distance, Point[] leftByY, Point[] rightByY) {
+        this.distance = distance;
+        this.rightByY = rightByY;
+        this.leftByY = leftByY;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "distance=" + distance +
+                ", leftByY=" + Arrays.toString(leftByY) +
+                ", rightByY=" + Arrays.toString(rightByY) +
+                "}";
+    }
+}
+
 
 public class Closest {
     public static double minimalDistance(int[] x, int y[]) {
@@ -52,59 +75,132 @@ public class Closest {
             points[i] = new Point(x[i], y[i]);
         }
 
-        Arrays.sort(points, Comparator.comparingInt(Point::getX));
-        double ans =  minimalDistance(points, 0, points.length);
+        Arrays.sort(points, comparingInt(Point::getX));
+        Result result = minimalDistance(points, 0, points.length);
 
-        return ans;
+        return result.distance;
     }
 
-    public static double minimalDistance(Point[] points, int left, int right) {
+    public static Result minimalDistance(Point[] points, int left, int right) {
 //        System.out.printf("Called %d-%d, %s\n", left, right, Arrays.toString(Arrays.copyOfRange(points, left, right)));
-        if(right - left < 4) {
-            return iteratveMinimalDistance(points, left, right, Double.POSITIVE_INFINITY);
+        if (right - left < 4) {
+            Result result = iterativePrepareResult(points, left, right, Double.POSITIVE_INFINITY);
+//            System.out.println("Returning " + result + "\n");
+            return result;
         }
 
         int pivot = (right + left) / 2;
 
-        double leftDistance = minimalDistance(points, left, pivot);
-        double rightDistance = minimalDistance(points, pivot, right);
+        Result leftResult = minimalDistance(points, left, pivot);
+        Result rightResult = minimalDistance(points, pivot, right);
 
-//        System.out.printf("%d-%d distance=%.3f\n", left, pivot, leftDistance);
-//        System.out.printf("%d-%d distance=%.3f\n", pivot, right, rightDistance);
+//        System.out.println("left = " + leftResult);
+//        System.out.println("right = " + rightResult);
 
-        Point pivotPoint = points[pivot];
-        Point[] leftPoints = Arrays.stream(points, left, pivot).filter(p -> pivotPoint.x - p.x <= leftDistance).toArray(Point[]::new);
-        Point[] rightPoints = Arrays.stream(points, pivot, right).filter(p -> p.x - pivotPoint.x <= rightDistance).toArray(Point[]::new);
+        // min distance
+        double distance = leftResult.distance < rightResult.distance
+                ? leftResult.distance
+                : rightResult.distance;
 
-//        System.out.printf("leftdistance=%.3f, leftPoints=%s\n", leftDistance, Arrays.toString(leftPoints));
-//        System.out.printf("leftdistance=%.3f, rightPoints=%s\n", rightDistance, Arrays.toString(rightPoints));
+        // filter
+        if(distance < leftResult.distance) {
+            leftResult.rightByY = filterByX(leftResult.rightByY, points[pivot - 1], distance);
+        }
+        if(distance < rightResult.distance) {
+            rightResult.leftByY = filterByX(rightResult.leftByY, points[pivot], distance);
+        }
 
-        leftPoints = joinToFirstArray(leftPoints, rightPoints);
+        // merge for strip
+        Point[] strip = mergeResult(leftResult.rightByY, rightResult.leftByY);
 
-        double distance = iteratveMinimalDistance(leftPoints, 0, leftPoints.length, Math.min(leftDistance, rightDistance));
-        return distance;
+        // search strip
+        for (int i = 0; i < strip.length; i++) {
+            for (int j = i + 1; j < strip.length && (strip[j].y - strip[i].y) < distance; j++) {
+                distance = min(distance, strip[i].distance(strip[j]));
+            }
+        }
+
+        // refilter after searching strip
+        if(distance < leftResult.distance) {
+            leftResult.leftByY = filterByX(leftResult.leftByY, points[left], distance);
+        }
+        if(distance < rightResult.distance) {
+            rightResult.rightByY = filterByX(rightResult.rightByY, points[right - 1], distance);
+        }
+
+//        System.out.println("Returning " + distance + "\n");
+        return new Result(distance, leftResult.leftByY, rightResult.rightByY);
+    }
+
+    private static Point[] filterByX(Point[] points, Point pivot, double distance) {
+        return Arrays.stream(points)
+                .filter(p -> Math.abs(pivot.x - p.x) < distance)
+                .toArray(Point[]::new);
+    }
+
+    public static Point[] mergeResult(Point[] rightByY, Point[] leftByY) {
+        Point[] result = new Point[rightByY.length + leftByY.length];
+
+        int l = 0, r = 0, i = 0;
+        while(r < rightByY.length && l < leftByY.length) {
+            if(leftByY[l].y < rightByY[r].y) {
+                result[i++] = leftByY[l++];
+            } else {
+                result[i++] = rightByY[r++];
+            }
+        }
+
+        while(l < leftByY.length) {
+            result[i++] = leftByY[l++];
+        }
+
+        while(r < rightByY.length) {
+            result[i++] = rightByY[r++];
+        }
+
+        return result;
     }
 
     public static <T> T[] joinToFirstArray(T[] leftPoints, T[] rightPoints) {
         int offset = leftPoints.length;
-        leftPoints = Arrays.copyOf(leftPoints, leftPoints.length + rightPoints.length);
+        T[] joined = Arrays.copyOf(leftPoints, leftPoints.length + rightPoints.length);
         for (int i = 0; i < rightPoints.length; i++) {
-            leftPoints[i + offset] = rightPoints[i];
+            joined[i + offset] = rightPoints[i];
         }
-        return leftPoints;
+        return joined;
     }
 
     public static double iteratveMinimalDistance(Point[] points, int left, int right, double distance) {
         for (int i = left; i < right - 1; i++) {
             Point pi = points[i];
-            for(int j = i + 1; j < right; j++) {
+            for (int j = i + 1; j < right; j++) {
                 double d = pi.distance(points[j]);
-                if(d < distance) {
+                if (d < distance) {
                     distance = d;
                 }
             }
         }
         return distance;
+    }
+
+    public static Result iterativePrepareResult(Point[] points, int left, int right, double distance) {
+        // solve iteratively
+        double sigma = iteratveMinimalDistance(points, left, right, distance);
+
+        int pivotLeft = points[left].x;
+        int pivotRight = points[right - 1].x;
+
+        Point[] leftByY = Arrays.stream(points, left, right)
+                .filter(p -> p.x - pivotLeft < sigma)
+                .sorted(comparingInt(Point::getY))
+                .toArray(Point[]::new);
+
+        Point[] rightByY = Arrays.stream(points, left, right)
+                .filter(p -> pivotRight - p.x < sigma)
+                .sorted(comparingInt(Point::getY))
+                .toArray(Point[]::new);
+
+        return new Result(sigma, leftByY, rightByY);
     }
 
 
